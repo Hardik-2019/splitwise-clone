11092da06e6478e1d6204f9753ad9b53f0224d41
Alla Jaswanth <allajaswanth@L-HGD2CX1KP6.local>
2025-11-19 14:48:05 +0530

    user service added

--- user-service/src/main/java/com/user_service/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/user_service/user_service/UserServiceApplication.java
@@ -2,7 +2,6 @@ package com.user_service.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class UserServiceApplication {
 
